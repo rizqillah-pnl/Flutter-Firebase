5eb504c7cc6d4a127407f5f535bdd81528abb9d9
FlutLab.io <>
2021-12-26 15:05:47 +0000

    Autosave

--- lib/main_page2.dart
+++ lib/main_page2.dart
@@ -34,7 +34,7 @@ import 'auth_services2.dart';
 // }
 
 // import 'package:flutter/material.dart';
-import 'nav-drawer.dart';
+// import 'nav-drawer.dart';
 
 // void main() {
 //   runApp(MaterialApp(
@@ -51,10 +51,9 @@ class NavigationDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("hello"),
+        title: Text(user.email),
       ),
-      drawer: DrawerWidget(),
-      endDrawer: DrawerWidget(),
+      drawer: sideBar(context),
       body: Center(
         child: Column(
           children: <Widget>[
@@ -71,6 +70,71 @@ class NavigationDrawer extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget sideBar(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          _drawerHeader(),
+          _drawerItem(icon: Icons.folder, text: 'My Files', onTap: () => print('Tap My Files')),
+          _drawerItem(icon: Icons.group, text: 'Shared with me', onTap: () => print('Tap Shared menu')),
+          _drawerItem(icon: Icons.access_time, text: 'Recent', onTap: () => print('Tap Recent menu')),
+          _drawerItem(icon: Icons.delete, text: 'Trash', onTap: () => print('Tap Trash menu')),
+          Divider(height: 25, thickness: 1),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, top: 10, bottom: 10),
+            child: Text(
+              "Labels",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
+              ),
+            ),
+          ),
+          _drawerItem(icon: Icons.bookmark, text: 'Family', onTap: () => print('Tap Family menu')),
+        ],
+      ),
+    );
+  }
+
+  Widget _drawerHeader() {
+    return UserAccountsDrawerHeader(
+      currentAccountPicture: ClipOval(
+        child: Image(image: AssetImage('assets/images/orang1.jpeg'), fit: BoxFit.cover),
+      ),
+      otherAccountsPictures: [
+        ClipOval(
+          child: Image(image: AssetImage('assets/images/orang2.jpg'), fit: BoxFit.cover),
+        ),
+        ClipOval(
+          child: Image(image: AssetImage('assets/images/orang3.jpeg'), fit: BoxFit.cover),
+        )
+      ],
+      accountName: Text(user.uid),
+      accountEmail: Text(user.email),
+    );
+  }
+
+  Widget _drawerItem({IconData icon, String text, GestureTapCallback onTap}) {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Icon(icon),
+          Padding(
+            padding: EdgeInsets.only(left: 25.0),
+            child: Text(
+              text,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+      onTap: onTap,
     );
   }
 }
