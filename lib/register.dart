48ae22ba77099be15a55f85c3fd683ddd61fbd6b
FlutLab.io <>
2021-12-26 10:05:51 +0000

    Autosave

--- lib/register.dart
+++ lib/register.dart
@@ -1,25 +1,16 @@
 import 'package:flutter/material.dart';
 import 'auth_services2.dart';
 import 'color.dart';
-import 'register.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Login Register Page",
-      initialRoute: "/",
-      routes: {
-        "/": (context) => LoginPage(),
-        RegisterPage.routeName: (context) => RegisterPage(),
-      },
-    ),
-  );
-}
-
-class LoginPage extends StatelessWidget {
+class RegisterPage extends StatelessWidget {
   TextEditingController user = TextEditingController(text: "");
   TextEditingController password1 = TextEditingController(text: "");
+  TextEditingController password2 = TextEditingController(text: "");
+  static const routeName = "/registerPage";
+  String password = null;
+  String username = null;
+  List akun = null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +22,7 @@ class LoginPage extends StatelessWidget {
             Center(
               child: Column(
                 children: <Widget>[
-                  _iconLogin(),
+                  _iconRegister(),
                   _titleDescription(),
                   _textField(),
                   _buildButton(context),
@@ -44,9 +35,9 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  Widget _iconLogin() {
+  Widget _iconRegister() {
     return Icon(
-      Icons.account_circle,
+      Icons.contact_mail_outlined,
       color: Colors.white,
       size: 150.0,
     );
@@ -59,7 +50,7 @@ class LoginPage extends StatelessWidget {
           padding: EdgeInsets.only(top: 16.0),
         ),
         Text(
-          "Login Into App",
+          "Registration",
           style: TextStyle(
             color: Colors.white,
             fontSize: 16.0,
@@ -142,6 +133,36 @@ class LoginPage extends StatelessWidget {
             autofocus: false,
           ),
         ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        Form(
+          autovalidate: true,
+          child: TextFormField(
+            validator: validatorConfirm,
+            controller: password2,
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: ColorPalette.underlineTextField,
+                  width: 1.5,
+                ),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.white,
+                  width: 3.0,
+                ),
+              ),
+              hintText: "Confirm Password",
+              hintStyle: TextStyle(color: ColorPalette.hintColor),
+            ),
+            style: TextStyle(color: Colors.white),
+            obscureText: true,
+            autofocus: false,
+          ),
+        ),
       ],
     );
   }
@@ -158,12 +179,12 @@ class LoginPage extends StatelessWidget {
             width: double.infinity,
             child: TextButton(
               child: Text(
-                "Login",
+                "Register",
                 style: TextStyle(color: ColorPalette.primaryColor),
                 textAlign: TextAlign.center,
               ),
               onPressed: () async {
-                await AuthServices.signIn(user.text, password1.text);
+                await AuthServices.signUp(user.text, password1.text);
               },
             ),
             decoration: BoxDecoration(
@@ -184,28 +205,40 @@ class LoginPage extends StatelessWidget {
         ),
         TextButton(
           child: Text(
-            "Register",
+            "Login",
             style: TextStyle(color: Colors.white),
           ),
           onPressed: () {
-            Navigator.pushNamed(context, "/registerPage");
+            Navigator.pushNamed(context, "/");
           },
         ),
       ],
     );
   }
-}
 
-String validatorUsername(String value) {
-  if (value.length < 3)
-    return 'Harus lebih 2 huruf';
-  else
-    return null;
-}
+  String validatorUsername(String value) {
+    if (value.length < 3) return 'Harus lebih 2 huruf';
 
-String validatorPassword(String value) {
-  if (value.length <= 5)
-    return 'Harus lebih 5 huruf';
-  else
+    username = value;
     return null;
+  }
+
+  String validatorPassword(String value) {
+    if (value.length <= 5) return 'Harus lebih 5 huruf';
+
+    password = value;
+    return null;
+  }
+
+  String validatorConfirm(String value) {
+    if (value == password) {
+      if (value.length <= 5) {
+        return 'Harus lebih 5 huruf';
+      } else {
+        return null;
+      }
+    } else {
+      return 'Password Konfirmasi Salah';
+    }
+  }
 }
