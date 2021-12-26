98c854e1d0c858adc7a621b37882dc99d9ccfa1d
FlutLab.io <>
2021-12-26 15:39:07 +0000

    Autosave

--- lib/main1-voting.dart
+++ lib/main1-voting.dart
@@ -27,25 +27,12 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('Mobil Favorite'),
       ),
-      body: Row(
-        children: <Widget>[
-          StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance.collection('mobil').snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) return LinearProgressIndicator();
-              return _buildList(context, snapshot.data.docs);
-            },
-          ),
-          Container(
-            height: 50,
-          ),
-          ElevatedButton(
-            child: Text("Kembali"),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ],
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('mobil').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return LinearProgressIndicator();
+          return _buildList(context, snapshot.data.docs);
+        },
       ),
     );
   }
@@ -72,20 +59,30 @@ class _MyHomePageState extends State<MyHomePage> {
     return Padding(
       key: ValueKey(record.name),
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          onTap: () => record.reference.update(
-            {
-              'vote': FieldValue.increment(1)
+      child: Row(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: ListTile(
+              title: Text(record.name),
+              trailing: Text(record.votes.toString()),
+              onTap: () => record.reference.update(
+                {
+                  'vote': FieldValue.increment(1)
+                },
+              ),
+            ),
+          ),
+          ElevatedButton(
+            child: Text("Kembali"),
+            onPressed: () {
+              Navigator.pop(context);
             },
           ),
-        ),
+        ],
       ),
     );
   }
