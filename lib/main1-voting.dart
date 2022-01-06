c9178b75f0dbf9a4802da6c2d629865a4f51adb8
FlutLab.io <>
2022-01-06 17:31:28 +0000

    Autosave

--- lib/main1-voting.dart
+++ lib/main1-voting.dart
@@ -40,7 +40,6 @@ class _MyHomePageState extends State<MyHomePage> {
         body: StreamBuilder<QuerySnapshot>(
           stream: FirebaseFirestore.instance.collection('mobil').snapshots(),
           builder: (context, snapshot) {
-            if (snapshot.hasData) return LinearProgressIndicator();
             if (!snapshot.hasData) return LinearProgressIndicator();
             return _buildList(context, snapshot.data.docs);
           },
