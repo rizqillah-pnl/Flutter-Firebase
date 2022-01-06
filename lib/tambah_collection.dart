26591d48e270f9cb0fe21b6b35aec902355531b6
FlutLab.io <>
2022-01-06 16:10:42 +0000

    Autosave

--- lib/tambah_collection.dart
+++ lib/tambah_collection.dart
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<TambahCollection> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Container(
-                  width: 200,
+                  width: 300,
                   child: TextField(
                     controller: data,
                     decoration: const InputDecoration(
