b21260eb47c60cb4cb4df4cc61b344ad85fd2ee4
FlutLab.io <>
2022-01-06 17:06:34 +0000

    Autosave

--- lib/main1-voting.dart
+++ lib/main1-voting.dart
@@ -8,6 +8,7 @@ class Voting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Favorite Car',
       home: MyHomePage(),
     );
