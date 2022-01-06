97e2621794d7611bdcaf73d90c97baa8ab4d9333
FlutLab.io <>
2022-01-06 15:27:29 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,14 @@ import 'auth_services2.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return StreamProvider.value(
@@ -18,3 +25,17 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamProvider.value(
+//       initialData: null,
+//       value: AuthServices.firebaseUserStream,
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: Wrapper(),
+//       ),
+//     );
+//   }
+// }
