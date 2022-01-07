47d11ae7a10890488d59e2c139e7f824915fc7c9
FlutLab.io <>
2022-01-07 02:21:46 +0000

    Autosave

--- lib/database_services.dart
+++ lib/database_services.dart
@@ -16,8 +16,10 @@ class DatabaseServices {
   static Future uploadImage(File imageFile) async {
     String fileName = basename(imageFile.path);
     FirebaseStorage storage = FirebaseStorage.instance;
+    print(storage);
     Reference ref = storage.ref().child(fileName + DateTime.now().toString());
     UploadTask task = ref.putFile(imageFile);
+    Storage
 
     task.whenComplete(() {
       return ref.getDownloadURL();
